ac0d8a6b7944ef1a608252c8f974266eba2bed6a
lucasramallo <lucasdebrito.jd@gmail.com>
2023-06-20 21:31:52 -0300

    Fibonacci

--- projects/Fibonacci/main.exs
+++ projects/Fibonacci/main.exs
@@ -1,33 +1,9 @@
-# defmodule Main do
-#   def run() do
-#     index_str = IO.gets("Digite o Index > ")
-#     index = String.trim(index_str) |> String.to_integer()
-#     result = fib(index - 1)
-#     IO.puts(result)
-#   end
-
-#   def fib(0), do: 0
-#   def fib(1), do: 1
-
-#   def fib(index) when index > 1 do
-#     fib(index - 1) + fib(index - 2)
-#   end
-# end
-
-# Main.run()
-
-
 defmodule Main do
   def run() do
     index_str = IO.gets("Digite o Index > ")
     index = String.trim(index_str) |> String.to_integer()
-
-    {time, result} = :timer.tc(fn -> fib(index - 1) end)
-
-    time_in_seconds = time / 1_000_000.0
-
-    IO.puts("Resultado: #{result}")
-    IO.puts("Tempo de execucao: #{time_in_seconds} segundos")
+    result = fib(index - 1)
+    IO.puts(result)
   end
 
   def fib(0), do: 0
@@ -39,3 +15,27 @@ defmodule Main do
 end
 
 Main.run()
+
+
+# defmodule Main do
+#   def run() do
+#     index_str = IO.gets("Digite o Index > ")
+#     index = String.trim(index_str) |> String.to_integer()
+
+#     {time, result} = :timer.tc(fn -> fib(index - 1) end)
+
+#     time_in_seconds = time / 1_000_000.0
+
+#     IO.puts("Resultado: #{result}")
+#     IO.puts("Tempo de execucao: #{time_in_seconds} segundos")
+#   end
+
+#   def fib(0), do: 0
+#   def fib(1), do: 1
+
+#   def fib(index) when index > 1 do
+#     fib(index - 1) + fib(index - 2)
+#   end
+# end
+
+# Main.run()
