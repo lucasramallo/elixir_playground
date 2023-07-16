1909b2c0f9a2f6c11c77d5e003bc234193a0e2b5
lucasramallo <lucasdebrito.jd@gmail.com>
2023-07-16 13:25:20 -0300

    Auth

---
+++ projects/Login/main.ex
@@ -0,0 +1,40 @@
+defmodule Main do
+  def run do
+    auth(getUser(), getPassword())
+  end
+
+  def getUser do
+    IO.gets("User > ")
+    |> String.trim()
+  end
+
+  def getPassword do
+    IO.gets("Password > ")
+    |> String.trim()
+  end
+
+  def auth(user, pass) when user == "lucas" and pass == "123" do
+    IO.puts("Authenticated!")
+  end
+
+  def auth(user, pass) when user == "lucas" and pass != "123" do
+    IO.puts("Invalid Password!")
+    run()
+  end
+
+  def auth(user, pass) when user != "lucas" and pass == "123" do
+    IO.puts("Invalid Username!")
+    run()
+  end
+
+  def auth(user, pass) when user != "lucas" and pass != "123" do
+    IO.puts("Invalid Username and Password!")
+    run()
+  end
+
+  def auth(:error) do
+    IO.puts("Error!")
+    run()
+  end
+
+end
