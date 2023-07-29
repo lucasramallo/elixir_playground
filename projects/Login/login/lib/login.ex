725d018fbbbb40f6aa7ef37272057fda46c2805d
lucasramallo <lucasdebrito.jd@gmail.com>
2023-07-28 22:22:53 -0300

    get

--- projects/Login/login/lib/login.ex
+++ projects/Login/login/lib/login.ex
@@ -1,7 +1,14 @@
 defmodule Login do
+
   @doc """
     Login Module
   """
+
+  def getCredentials do
+    IO.gets("Name > ")
+    |> String.trim()
+  end
+
   def auth do
     credentials = {"lucas", "1234"}
     case credentials do
