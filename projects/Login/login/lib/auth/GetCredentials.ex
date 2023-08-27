a7f64a12802589afc89888392201e8c9592959bb
lucasramallo <lucasdebrito.jd@gmail.com>
2023-08-27 17:44:50 -0300

    tetse

--- projects/Login/login/lib/auth/GetCredentials.ex
+++ projects/Login/login/lib/auth/GetCredentials.ex
@@ -1,13 +1,13 @@
 defmodule GetCredentials do
   def fetch_name() do
     IO.gets("Name > ")
-    |> String.trim()
+    #|> String.trim()
     |> String.downcase()
   end
 
   def fetch_password() do
     IO.gets("Password > ")
-    |> String.trim()
+    #|> String.trim()
     |> String.downcase()
   end
 end
