d0fc1bb839caf061dcb42fe2cf33a3e9b6458881
lucasramallo <lucasdebrito.jd@gmail.com>
2023-11-03 11:33:10 -0300

    Login: Fix Input

--- projects/Login/login/lib/auth/Input.ex
+++ projects/Login/login/lib/auth/Input.ex
@@ -1,8 +1,8 @@
 defmodule Input do
   def fetch_name() do
     IO.gets("Name > ")
-    #|> String.trim()
-    #|> String.downcase()
+    |> String.trim()
+    |> String.downcase()
   end
 
   def fetch_password() do
